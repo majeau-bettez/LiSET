e92044cd335a6a6e640713482b58fab1a1c7b43a
Guillaume Majeau-Bettez <joie.bill@gmail.com>
2018-06-01 07:51:34 -0400

    start packaging
     Sur la branche master
     Votre branche est en avance sur 'origin/master' de 1 commit.
       (utilisez "git push" pour publier vos commits locaux)
    
     Modifications qui seront validées :
    	nouveau fichier : MANIFEST.in
    	modifié :         liset_clusters/__init__.py
    	nouveau fichier : liset_clusters/version.py
    	nouveau fichier : setup.py

---
+++ liset_clusters/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+from liset_clusters.liset_cluster import *
+
+__author__ = 'Guillaume Majeau-Bettez'
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+exec(open('liset_clusters/version.py').read())
+
+setup(
+        name='liset_clusters',
+        description= 'A simple clustering tool to assist Lifecycle Screening of Emerging Technologies (LiSET)', 
+        long_description=open('README.md').read(),
+        url='https://github.com/majeau-bettez/LiSET',
+        author='Guillaume Majeau-Bettez',
+        author_email='guillaume.majeau-bettez@ntnu.no',
+        version=__version__,
+        packages=['liset_clusters',],
+        install_requires = ['numpy >= 1.14.2',
+                            'pandas >= 0.22.0',
+                            'scipy >= 1.0.0',
+                            'jenkspy >= 0.1.4',
+                            'matplotlib >=2.1.2'],
+        license='GPLv3',
+    )
